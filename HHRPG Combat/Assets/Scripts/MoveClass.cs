5551d065327ba05b680071ded9e61ab07df4aea3
brennanmcculloch <mcculloch.brennan@gmail.com>
2021-03-16 14:06:59 -0700

    Starting

---
+++ HHRPG Combat/Assets/Scripts/MoveClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClass : MonoBehaviour
+{
+    protected string moveName;
+    protected string type; //attack, rhythm, support
+    protected bool group; //true if group effect, false if not
+    protected int cost;
+    protected bool friendly; //true if only cast on party, false if cast on enemy
+
+    MoveClass(string _move, string _type, bool _group, int _cost, bool _friend)
+    {
+        moveName = _move;
+        type = _type;
+        group = _group;
+        cost = _cost;
+        friendly = _friend;
+    }
+}
---
+++ HHRPG Combat/Assets/Scripts/PartyMemberClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMemberClass : MonoBehaviour
+{
+    public string memberName;
+    public Dictionary<string, string> affinities;
+    public Dictionary<string, int> stats;
+    public MoveClass[] moves; //MAKE THIS SIZE 8
+
+    PartyMemberClass(string _name, Dictionary<string, string> _aff, Dictionary<string, int> _stats, MoveClass[] _moves)
+    {
+        memberName = _name;
+        affinities = _aff;
+        stats = _stats;
+        moves = _moves;
+    }
+}
