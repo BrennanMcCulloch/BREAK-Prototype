065b700ce1e055a2a74d24d44cc9fc8c44c468d5
BrennanMcCulloch <bmccullo@ucsc.edu>
2021-04-20 10:39:45 -0700

    Did some minor work, and also set up documentation

--- HHRPG Combat/Assets/Scripts/Learning Stuff/NoteObject.cs
+++ HHRPG Combat/Assets/Scripts/Learning Stuff/NoteObject.cs
@@ -6,17 +6,24 @@ public class NoteObject : MonoBehaviour
 {
 
     public bool canBePressed;
+    public KeyCode keyToPress;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //19:24
+        if(Input.GetKeyDown(keyToPress)) 
+        {
+            if(canBePressed) 
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
